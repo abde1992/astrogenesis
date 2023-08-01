590790915ce269e2b1aed0968573913b4f91b03c
Abde Ali Lokhandwala <iaabde1992@gmail.com>
2023-08-01 20:08:04 +0200

    Allow different types of terrain modifications, add the ability to plant new trees

--- modules/gameplay.js
+++ modules/gameplay.js
@@ -1,8 +1,13 @@
 import * as THREE from 'three';
 
-export function setupGameplay(scene, terrain, camera) {
+export function setupGameplay(scene, terrain, camera, resources) {
     const raycaster = new THREE.Raycaster();
     const mouse = new THREE.Vector2();
+
+    let inventory = {
+        seeds: 10, // Player starts with 10 seeds
+        tool: 'raise' // Default tool is the 'raise' tool
+    };
 
     window.addEventListener('mousemove', onMouseMove, false);
     window.addEventListener('click', onTerrainClick, false);
@@ -19,32 +24,60 @@ export function setupGameplay(scene, terrain, camera) {
         if (intersects.length > 0) {
             const intersect = intersects[0];
 
-            // Use an easing function to smoothly change the height over time
-            let startHeight = intersect.face.vertexNormals[0].y;
-            let endHeight = startHeight + 0.1;
-            let startTime = Date.now();
-            let duration = 1000; // Change will occur over 1 second
+            // Perform action based on current tool
+            if (inventory.tool === 'raise' || inventory.tool === 'lower') {
+                let startHeight = intersect.face.vertexNormals[0].y;
+                let endHeight = inventory.tool === 'raise' ? startHeight + 0.1 : startHeight - 0.1;
+                let startTime = Date.now();
+                let duration = 1000; // Change will occur over 1 second
 
-            function updateHeight() {
-                let time = Date.now();
-                let timeFraction = (time - startTime) / duration;
+                function updateHeight() {
+                    let time = Date.now();
+                    let timeFraction = (time - startTime) / duration;
 
-                if (timeFraction < 1) {
-                    requestAnimationFrame(updateHeight);
-                } else {
-                    timeFraction = 1;
+                    if (timeFraction < 1) {
+                        requestAnimationFrame(updateHeight);
+                    } else {
+                        timeFraction = 1;
+                    }
+
+                    let height = startHeight + (endHeight - startHeight) * timeFraction;
+                    intersect.face.vertexNormals[0].y = height;
+                    intersect.face.vertexNormals[1].y = height;
+                    intersect.face.vertexNormals[2].y = height;
+
+                    terrain.geometry.normalsNeedUpdate = true;
+                    terrain.geometry.verticesNeedUpdate = true;
                 }
 
-                let height = startHeight + (endHeight - startHeight) * timeFraction;
-                intersect.face.vertexNormals[0].y = height;
-                intersect.face.vertexNormals[1].y = height;
-                intersect.face.vertexNormals[2].y = height;
+                updateHeight();
+            } else if (inventory.tool === 'plant' && inventory.seeds > 0) {
+                // Use the resource placement code to add a new tree
+                const geometry = new THREE.ConeGeometry(0.2, 0.5);
+                const material = new THREE.MeshBasicMaterial({ color: 0x8B4513 });
+                const tree = new THREE.Mesh(geometry, material);
+                tree.position.copy(intersect.point);
 
-                terrain.geometry.normalsNeedUpdate = true;
-                terrain.geometry.verticesNeedUpdate = true;
+                scene.add(tree);
+                resources.trees.push(tree);
+
+                // Reduce the number of seeds in the inventory
+                inventory.seeds--;
             }
-
-            updateHeight();
         }
     }
+
+    // Function to change the current tool
+    function changeTool(newTool) {
+        if (['raise', 'lower', 'plant'].includes(newTool)) {
+            inventory.tool = newTool;
+        }
+    }
+
+    // Function to add seeds to the inventory
+    function addSeeds(number) {
+        inventory.seeds += number;
+    }
+
+    return { changeTool, addSeeds };
 }
