5c7866cbaee05a1afc0f9ec0f2d0f67e368f073c
Abde Ali Lokhandwala <iaabde1992@gmail.com>
2023-08-01 20:13:57 +0200

    Handle game start and tool selection

--- main.js
+++ main.js
@@ -1,19 +1,57 @@
-import { setupScene, animate } from './modules/sceneSetup.js';
-import { setupTerrain } from './modules/terrain.js';
-import { setupAtmosphere } from './modules/atmosphere.js';
-import { setupResources } from './modules/resources.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { generatePlanet } from './modules/planet.js';
 import { setupGameplay } from './modules/gameplay.js';
+import { setupResources } from './modules/resources.js';
 
-// Set up the scene, camera, renderer, and controls
-const { scene, camera, renderer, controls } = setupScene();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls(camera, renderer.domElement);
 
-// Add the terrain, atmosphere, and resources to the scene
-const terrain = setupTerrain(scene);
-setupAtmosphere(scene);
-setupResources(scene, terrain);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('game-container').appendChild(renderer.domElement);
 
-// Set up the gameplay mechanics
-setupGameplay(scene, terrain, camera);
+camera.position.z = 5;
 
-// Start the animation loop
-animate(renderer, scene, camera);
+function setupGame() {
+    // Create the planet
+    const terrainData = fetchTerrainData();
+    const atmosphereData = fetchAtmosphereData();
+    const planet = generatePlanet(terrainData, atmosphereData);
+    scene.add(planet);
+
+    // Create the resources
+    const resourcesData = fetchResourcesData();
+    const resources = setupResources(resourcesData, planet);
+    resources.forEach(resource => scene.add(resource));
+
+    // Setup gameplay
+    const gameplay = setupGameplay(scene, planet, camera, resources);
+
+    // Handle tool selection
+    document.getElementById('tool-raise').addEventListener('click', () => {
+        gameplay.changeTool('raise');
+    });
+
+    document.getElementById('tool-lower').addEventListener('click', () => {
+        gameplay.changeTool('lower');
+    });
+
+    document.getElementById('tool-plant').addEventListener('click', () => {
+        gameplay.changeTool('plant');
+    });
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+animate();
+
+document.getElementById('start-button').addEventListener('click', () => {
+    document.getElementById('title-screen').style.display = 'none';
+    setupGame();
+});
